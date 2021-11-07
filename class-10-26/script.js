bf1dded9c6163225951999f41e655d7e272228c7
sak2212 <sak2212@columbia.edu>
2021-11-07 10:20:25 -0500

    project03

---
+++ class-10-26/script.js
@@ -0,0 +1,15 @@
+/*one*/
+// function sayHello() {}
+
+/*exercise*/
+// let age = 25;
+// let height = 5;
+// console.log(age);
+// console.log(height);
+
+/*part one*/
+let noun = "pie";
+let adjective = "spooky";
+let noun2 = "ghouls";
+let sentence = `My ${noun} brings all the ${adjective} ${noun2} to the yard.`
+console.log(sentence) 
